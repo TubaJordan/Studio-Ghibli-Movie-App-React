9bf29c7755c33b9274782fe1b0d8e3b27def39ff
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-07-11 00:07:06 -0700

    car layout updated

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,13 +3,15 @@ import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-        <Card className="h-100 bg-light" onClick={() => onMovieClick(movie)}>
-            <Card.Img variant="top" src={movie.imageUrl} />
-            <Card.Body className="text-center">
-                <Card.Title>{movie.title}</Card.Title>
+        <Card className="h-100 custom-card p-3 movieCard" onClick={() => onMovieClick(movie)}>
+            <Card.Text className="mt-1 cardTop"></Card.Text>
+            <Card.Title className="text-center cardTitle">{movie.title}</Card.Title>
+            <Card.Img className="mt-2 cardImage" src={movie.imageUrl} />
+            <Card.Body className="text-center cardGenre d-flex">
+                <Card.Text>{movie.releaseYear}</Card.Text>
                 <Card.Text>{`${movie.genres.name.charAt(0).toUpperCase()}${movie.genres.name.slice(1)}`}</Card.Text>
             </Card.Body>
-        </Card>
+        </Card >
     );
 };
 
@@ -18,6 +20,7 @@ MovieCard.propTypes = {
         _id: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
         imageUrl: PropTypes.string.isRequired,
+        releaseYear: PropTypes.string.isRequired,
         director: PropTypes.shape({
             name: PropTypes.string.isRequired
         }).isRequired,
