dde6d603bbccd4b3ee024e3a42342e0bcc215227
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-07-13 22:35:24 -0700

    Link added

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,21 @@
+import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
     return (
-        <Card className="h-100 custom-card p-3 movieCard" onClick={() => onMovieClick(movie)}>
-            <Card.Text className="mt-1 cardTop"></Card.Text>
-            <Card.Title className="text-center cardTitle">{movie.title}</Card.Title>
-            <Card.Img className="mt-2 cardImage" src={movie.imageUrl} />
-            <Card.Body className="text-center cardGenre d-flex">
-                <Card.Text>{movie.releaseYear}</Card.Text>
-                <Card.Text>{`${movie.genres.name.charAt(0).toUpperCase()}${movie.genres.name.slice(1)}`}</Card.Text>
-            </Card.Body>
-        </Card >
+        <Link to={`/movies/${encodeURIComponent(movie._id)}`} className="links">
+            <Card className="h-100 custom-card p-3 movieCard" >
+                <Card.Text className="mt-1 cardTop"></Card.Text>
+                <Card.Title className="text-center cardTitle">{movie.title}</Card.Title>
+                <Card.Img className="mt-2 cardImage" src={movie.imageUrl} />
+                <Card.Body className="text-center cardGenre d-flex">
+                    <Card.Text>{movie.releaseYear}</Card.Text>
+                    <Card.Text>{`${movie.genres.name.charAt(0).toUpperCase()}${movie.genres.name.slice(1)}`}</Card.Text>
+                </Card.Body>
+            </Card >
+        </Link>
     );
 };
 
@@ -29,5 +33,4 @@ MovieCard.propTypes = {
             name: PropTypes.string.isRequired
         }).isRequired
     }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
 };
