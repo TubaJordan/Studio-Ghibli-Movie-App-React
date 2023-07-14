58119ba1e52edf0b8a68f380b0cb232bd7b4cdf8
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-07-13 22:36:46 -0700

    base navigation bar added

---
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -0,0 +1,37 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export const NavigationBar = ({ user, onLoggedOut }) => {
+    return (
+        <Navbar expand="lg" className="navBar">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    NAME PLACEHOLDER
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="justify-content-end" style={{ width: "100%" }}>
+                        {!user && (
+                            <>
+                                <Nav.Link as={Link} to="/login" className="text-right">
+                                    Login
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/signup" className="text-right">
+                                    Signup
+                                </Nav.Link>
+                            </>
+                        )}
+                        {user && (
+                            <>
+                                <Nav.Link as={Link} to="/">
+                                    Home
+                                </Nav.Link>
+                                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar >
+    );
+};
