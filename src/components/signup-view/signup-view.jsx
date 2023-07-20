1630954cc97b4df7e1f4f168d6724294d0bf67e7
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-07-19 23:34:15 -0700

    form validation added

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { FloatingLabel } from "react-bootstrap";
+import { FloatingLabel, Alert } from "react-bootstrap";
 
 export const SignupView = () => {
 
@@ -10,6 +10,37 @@ export const SignupView = () => {
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
     const [birthDate, setBirthDate] = useState("");
+
+    const [usernameValid, setUsernameValid] = useState(true);
+    const [passwordValid, setPasswordValid] = useState(true);
+    const [emailValid, setEmailValid] = useState(true);
+    const [birthDateValid, setBirthDateValid] = useState(true);
+
+    const validateFormUsername = () => {
+        const isUsernameValid = username.length >= 5;
+        setUsernameValid(isUsernameValid);
+        return isUsernameValid;
+    }
+
+    const validateFormPassword = () => {
+        const isPasswordValid = password.length >= 6;
+        setPasswordValid(isPasswordValid);
+        return isPasswordValid
+    }
+
+    const validateFormEmail = () => {
+        const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+        setEmailValid(isEmailValid);
+        return isEmailValid;
+    }
+
+    const validateFormBirthDate = () => {
+        const isBirthDateValid = birthDate !== "";
+        setBirthDateValid(isBirthDateValid);
+        return isBirthDateValid;
+    }
+
+
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -34,14 +65,25 @@ export const SignupView = () => {
             } else {
                 alert("Signup failed");
             }
-        });
+        })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+
     };
+
+
+
 
     return (
         <Form onSubmit={handleSubmit}>
-            <Form.Label className="fs-3 fw-semibold mt-3 p-2 text-center loginLabels" style={{ width: "100%" }}>Register A New Account</Form.Label>
+
+            <Form.Label className="fs-3 fw-semibold mt-3 p-2 text-center loginLabels" style={{ width: "100%" }}>
+                Register A New Account
+            </Form.Label>
 
             <Form.Group controlId="signupUsername">
+
                 <FloatingLabel
                     controlId="signupUsername"
                     label="Username"
@@ -51,13 +93,21 @@ export const SignupView = () => {
                         type="text"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
+                        onBlur={validateFormUsername}
+                        isInvalid={!usernameValid}
                         required
-                        minLength={3}
+                        minLength={5}
                     />
+
+                    <Form.Control.Feedback type="invalid">
+                        Username must be at least 5 characters
+                    </Form.Control.Feedback>
+
                 </FloatingLabel>
             </Form.Group>
 
             <Form.Group controlId="signupPassword">
+
                 <FloatingLabel
                     controlId="signupPassword"
                     label="Password"
@@ -67,8 +117,15 @@ export const SignupView = () => {
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                        onBlur={validateFormPassword}
+                        isInvalid={!passwordValid}
                         required
                     />
+
+                    <Form.Control.Feedback type="invalid">
+                        Password must be at least 6 characters
+                    </Form.Control.Feedback>
+
                 </FloatingLabel>
             </Form.Group>
 
@@ -82,8 +139,15 @@ export const SignupView = () => {
                         type="email"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
+                        onBlur={validateFormEmail}
+                        isInvalid={!emailValid}
                         required
                     />
+
+                    <Form.Control.Feedback type="invalid">
+                        Enter a valid email address
+                    </Form.Control.Feedback>
+
                 </FloatingLabel>
             </Form.Group>
 
@@ -97,8 +161,15 @@ export const SignupView = () => {
                         type="date"
                         value={birthDate}
                         onChange={(e) => setBirthDate(e.target.value)}
+                        onBlur={validateFormBirthDate}
+                        isInvalid={!birthDateValid}
                         required
                     />
+
+                    <Form.Control.Feedback type="invalid">
+                        Select your birthday
+                    </Form.Control.Feedback>
+
                 </FloatingLabel>
             </Form.Group>
 
