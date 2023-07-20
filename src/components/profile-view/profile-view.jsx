c356368b1a4b0dc03da2ea11bb60c6f4dd3ab400
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-07-19 23:34:07 -0700

    form validation added

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -86,6 +86,37 @@ export const ProfileView = ({ user, token, movies, onLoggedOut }) => {
     const formattedDate = `${getMonthInWords(newDate.getUTCMonth() + 1)} ${getDayInWords(newDate.getUTCDate())}, ${newDate.getUTCFullYear()}`;
 
 
+    const [usernameValid, setUsernameValid] = useState(true);
+    const [passwordValid, setPasswordValid] = useState(true);
+    const [emailValid, setEmailValid] = useState(true);
+    const [birthDateValid, setBirthDateValid] = useState(true);
+
+    const validateFormUsername = () => {
+        const isUsernameValid = username.length >= 5;
+        setUsernameValid(isUsernameValid);
+        return isUsernameValid;
+    }
+
+    const validateFormPassword = () => {
+        const isPasswordValid = password.length >= 6;
+        setPasswordValid(isPasswordValid);
+        return isPasswordValid
+    }
+
+    const validateFormEmail = () => {
+        const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+        setEmailValid(isEmailValid);
+        return isEmailValid;
+    }
+
+    const validateFormBirthDate = () => {
+        const isBirthDateValid = birthDate !== "";
+        setBirthDateValid(isBirthDateValid);
+        return isBirthDateValid;
+    }
+
+
+
     return (
         <>
             <h1 className="text-center fancyText mb-3 profileBanner">Profile</h1>
@@ -113,9 +144,15 @@ export const ProfileView = ({ user, token, movies, onLoggedOut }) => {
                                     type="text"
                                     value={username}
                                     onChange={(e) => setUsername(e.target.value)}
+                                    onBlur={validateFormUsername}
+                                    isInvalid={!usernameValid}
                                     required
                                     minLength={5}
                                 />
+                                <Form.Control.Feedback type="invalid">
+                                    Username must be at least 5 characters
+                                </Form.Control.Feedback>
+
                             </FloatingLabel>
                         </Form.Group>
 
@@ -129,8 +166,14 @@ export const ProfileView = ({ user, token, movies, onLoggedOut }) => {
                                     type="password"
                                     value={password}
                                     onChange={(e) => setPassword(e.target.value)}
+                                    onBlur={validateFormPassword}
+                                    isInvalid={!passwordValid}
                                     required
                                 />
+                                <Form.Control.Feedback type="invalid">
+                                    Password must be at least 6 characters
+                                </Form.Control.Feedback>
+
                             </FloatingLabel>
                         </Form.Group>
 
@@ -144,8 +187,14 @@ export const ProfileView = ({ user, token, movies, onLoggedOut }) => {
                                     type="email"
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
+                                    onBlur={validateFormEmail}
+                                    isInvalid={!emailValid}
                                     required
                                 />
+                                <Form.Control.Feedback type="invalid">
+                                    Enter a valid email address
+                                </Form.Control.Feedback>
+
                             </FloatingLabel>
                         </Form.Group>
 
@@ -159,8 +208,14 @@ export const ProfileView = ({ user, token, movies, onLoggedOut }) => {
                                     type="date"
                                     value={birthDate}
                                     onChange={(e) => setBirthDate(e.target.value)}
+                                    onBlur={validateFormBirthDate}
+                                    isInvalid={!birthDateValid}
                                     required
                                 />
+                                <Form.Control.Feedback type="invalid">
+                                    Select your birthday
+                                </Form.Control.Feedback>
+
                             </FloatingLabel>
                         </Form.Group>
 
