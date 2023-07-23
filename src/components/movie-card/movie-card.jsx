f4490fea4704147a51102f51a816f8029ba04683
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-07-22 19:06:16 -0700

    accessibility updates

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { useParams } from "react-router";
-import { useEffect, useState } from "react";
+
 
 export const MovieCard = ({ movie }) => {
 
+
     return (
 
-        <Link to={`/movies/${encodeURIComponent(movie._id)}`} className="links">
+        <Link to={`/movies/${encodeURIComponent(movie._id)}`} className="links" aria-label={`View details for ${movie.title}`} title={movie.title}>
             <Card className="h-100 custom-card p-3 movieCard" >
-                <Card.Text className="mt-1 cardTop"></Card.Text>
+                <Card.Text className="mt-1 cardTop" aria-hidden="true"></Card.Text>
                 <Card.Title className="text-center cardTitle">{movie.title}</Card.Title>
-                <Card.Img className="mt-2 cardImage" src={movie.imageUrl} />
+                <Card.Img className="mt-2 cardImage" src={movie.imageUrl} alt={movie.title} />
                 <Card.Body className="text-center cardGenre d-flex">
                     <Card.Text>{movie.releaseYear}</Card.Text>
                     <Card.Text>{`${movie.genres.name.charAt(0).toUpperCase()}${movie.genres.name.slice(1)}`}</Card.Text>
                 </Card.Body>
 
             </Card >
-        </Link>
+        </Link >
     );
 };
 
