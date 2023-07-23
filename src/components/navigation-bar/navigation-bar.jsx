b0bae37b8ead085f52666bca155364bd951ed2c4
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-07-22 19:06:28 -0700

    accessibility updates

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,11 +1,9 @@
 import { Navbar, Container, Nav, Dropdown, DropdownButton } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button"
-import { useEffect, useState } from "react";
-import PropTypes from "prop-types";
 import { useLocation } from "react-router-dom"
-import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
+import topLogo from "./top-logo.png";
+
 
 export const NavigationBar = ({ user, onLoggedOut, onSearch, searchQuery, handleSortOptionSelect, sortOptions, currentSortOption }) => {
 
@@ -17,34 +15,39 @@ export const NavigationBar = ({ user, onLoggedOut, onSearch, searchQuery, handle
         );
     };
 
+
     return (
         <Navbar expand="lg" className="navBar">
             <Container>
-                <Navbar.Brand as={Link} to="/">
-                    NAME PLACEHOLDER
+
+                <Navbar.Brand as={Link} to="/" className="logoBrand" title="Ghibli Movie Collection">
+                    <img src={topLogo} alt="Ghibli Movie Collection Logo" className="navBarImg" role="img" aria-label="Ghibli Movie Collection Logo" />
+                    <span className="logoHeader">Ghibli Movie Collection</span>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="justify-content-end" style={{ width: "100%" }}>
                         {!user && (
                             <>
-                                <Nav.Link as={Link} to="/login" className="text-right">
+                                <Nav.Link as={Link} to="/login" className="text-right navBarItem" role="link" title="Login">
                                     Login
                                 </Nav.Link>
-                                <Nav.Link as={Link} to="/signup" className="text-right">
+                                <Nav.Link as={Link} to="/signup" className="text-right navBarItem" role="link" title="Signup">
                                     Signup
                                 </Nav.Link>
                             </>
                         )}
                         {user && (
                             <>
-                                <Nav.Link as={Link} to="/">
+                                <Nav.Link as={Link} to="/" className="navBarItem" role="link" title="Home">
                                     Home
                                 </Nav.Link>
-                                <Nav.Link as={Link} to="/profile">
+                                <Nav.Link as={Link} to="/profile" className="navBarItem" role="link" title="Profile">
                                     Profile
                                 </Nav.Link>
-                                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                                <Nav.Link onClick={onLoggedOut} className="navBarItem" role="button" title="Logout">
+                                    Logout
+                                </Nav.Link>
 
                                 {!isMovieOrProfileView() && (
                                     <Form className="d-flex">
@@ -53,10 +56,12 @@ export const NavigationBar = ({ user, onLoggedOut, onSearch, searchQuery, handle
                                             placeholder="Search"
                                             className="me-2 mt-1 mb-1"
                                             aria-label="Search"
+                                            role="search"
                                             value={searchQuery}
                                             onChange={(e) => {
                                                 onSearch(e.target.value)
                                             }}
+                                            title="Search For A Movie By Title"
                                         />
                                     </Form>
                                 )}
@@ -68,6 +73,7 @@ export const NavigationBar = ({ user, onLoggedOut, onSearch, searchQuery, handle
                                         onSelect={handleSortOptionSelect}
                                         className="sortButton mt-1 mb-1"
                                         align={"end"}
+                                        role="button"
                                     >
 
                                         {sortOptions.map((option) => (
@@ -76,6 +82,7 @@ export const NavigationBar = ({ user, onLoggedOut, onSearch, searchQuery, handle
                                                 eventKey={option.value}
                                                 active={currentSortOption === option.value}
                                                 className="sortItems mt-1"
+                                                role="menuitem"
                                             >
                                                 {option.label}
                                             </Dropdown.Item>
